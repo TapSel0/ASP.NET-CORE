50b21a342b9e6d77a9eb0a4887e536e23367d6e3
Денис Врацкий <dvratskiy@mail.ru>
2024-09-02 15:51:50 +0700

    Add validation in create view

--- Models/Car.cs
+++ Models/Car.cs
@@ -7,17 +7,18 @@ namespace asp_gpt.Models
     {
         public int Id { get; set; }
 
-        [Required]
         [StringLength(255)]
+        [Required(ErrorMessage = "Имя обязательно для заполнения")]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Цена обязательно для заполнения")]
+        [Range(0.01, 1000000.00, ErrorMessage = "Цена должна быть в диапазоне от 0.01 до 1000000.00")]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
 
-        [Required]
-        [StringLength(255)]
+        [Required(ErrorMessage = "Описание обязательно для заполнения")]
+        [StringLength(255, ErrorMessage = "Описание должно быть короче 255 символов")]
         public string Description { get; set; }
     }
 }
